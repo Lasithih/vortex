8ee68898b02953343301c0848cfb431fdfe7bbea
Lasith Hettiarachchi <lasith.h@fidenz.com>
2021-05-16 02:58:35 +0530

    login

---
+++ api/api.py
@@ -0,0 +1,8 @@
+from re import A
+from flask import request
+
+def create_endpoints(app):
+    create_endpoint_login(app)
+
+def create_endpoint_login(app):
+    pass
---
+++ static/js/login.js
@@ -0,0 +1,31 @@
+function login() {
+    var email = $("#login-username").val();
+    if(email === '' || email === null) {
+        alert("Please enter your username");
+        return;
+    }
+
+    var password = $("#login-password").val();
+    if(password === '' || password === null) {
+        alert("Please enter your password");
+        return;
+    }
+
+    $.ajax({
+        url: "/api/v1/auth/login", //the page containing php script
+        type: "POST", //request type,
+        dataType: 'json',
+        data: { username: email, password: password },
+        success: function(result) {
+            if(result.success) {
+                window.location.href = '/';
+                
+            } else {
+                alert("Username or password is incorrect");
+            }
+        },
+        error(e) {
+            console.log(e.responseText);
+        }
+    });
+}
